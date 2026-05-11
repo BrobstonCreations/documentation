7bafcaa7ca6ebeeb508f1cd1f3c5860c1d01e362
Tony Brobston <anthonybrobston@gmail.com>
2026-05-11 14:59:26 -0500

    Fix github action

--- scripts/generate-pdfs.sh
+++ scripts/generate-pdfs.sh
@@ -7,5 +7,5 @@ for file in install-instructions/*.MD; do
     filename="${filename%.*}"
 
     echo "Processing: $file"
-    md-to-pdf "$file"
+    md-to-pdf "$file" --launch-options '{ "args": ["--no-sandbox"] }'
 done
